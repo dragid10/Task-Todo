071f725678878ee93519162d8579f4fb44982608
Alex Oladele <dragid10@gmail.com>
2019-12-26 14:40:24 -0500

    gitflow-feature-stash: write-tests

---
+++ app/src/test/java/com/example/task_todo/test/TaskPresenterTest.kt
@@ -0,0 +1,28 @@
+package com.example.task_todo.test
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class TaskPresenterTest {
+//    private lateinit var presenter: TaskContract.Presenter<>
+    @BeforeEach
+    fun setUp() {
+
+    }
+
+    @Test
+    fun startCreateTask() {
+    }
+
+    @Test
+    fun saveTaskDetails() {
+    }
+
+    @Test
+    fun deleteTask() {
+    }
+
+    @Test
+    fun getTaskList() {
+    }
+}
